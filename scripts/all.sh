9592da7c3d58b2834fb6e9efcbd68e60d3f64c2d
Xavier Maysonnave <xavier@andaman.blue.av>
2019-09-16 10:29:58 +0530

    bundle pdf in base and cardo output

--- scripts/all.sh
+++ scripts/all.sh
@@ -2,5 +2,16 @@
 ./scripts/prepare.sh
 # browserify -s _ ipfs-saver.js -o ipfs/plugins/ipfs/ipfs-saver.js || exit 1
 browserify src/ipfs-saver.js -s ipfsSaver -o ipfs/plugins/ipfs/ipfs-saver.js || exit 1
-tiddlywiki ipfs --output ipfs/output/base --build --verbose || exit 1
-tiddlywiki ipfs --import tw5-cardo/Cardo-1.0.4.json application/json --output ipfs/output/cardo --build --verbose || exit 1
+tiddlywiki ipfs \
+  --import attachment/vol_10_e.json application/json \
+  --import attachment/vol_10_f.json application/json \
+  --output ipfs/output/base \
+  --build \
+  --verbose || exit 1
+tiddlywiki ipfs \
+  --import attachment/vol_10_e.json application/json \
+  --import attachment/vol_10_f.json application/json \
+  --import tw5-cardo/Cardo-1.0.4.json application/json \
+  --output ipfs/output/cardo \
+  --build \
+  --verbose || exit 1
