3eaf4910caa0c93b1ccba312df00a058c7398f2e
Xavier Maysonnave <x.maysonnave@gmail.com>
2019-10-08 15:17:15 +0530

    - import .tid instead of .json - preliminary documentation - splash screen

--- scripts/all.sh
+++ scripts/all.sh
@@ -3,16 +3,10 @@
 # browserify -s _ ipfs-library.js -o ipfs/plugins/ipfs/ipfs-library.js || exit 1
 npx browserify src/ipfs-library.js -s IpfsLibrary -o ipfs/plugins/ipfs/ipfs-library.js || exit 1
 npx tiddlywiki ipfs \
-  --import attachment/the_little_prince_embedded.json application/json \
-  --import attachment/the_little_prince_ipfs_encrypted.json application/json \
-  --import attachment/the_little_prince_ipfs.json application/json \
   --output ipfs/output/base \
   --build \
   --verbose || exit 1
 npx tiddlywiki ipfs \
-  --import attachment/the_little_prince_embedded.json application/json \
-  --import attachment/the_little_prince_ipfs_encrypted.json application/json \
-  --import attachment/the_little_prince_ipfs.json application/json \
   --import tw5-cardo/Cardo-1.0.4.json application/json \
   --output ipfs/output/cardo \
   --build \
