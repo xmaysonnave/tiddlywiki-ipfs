81620075a9b1972b5be42204794e3cdac92ac3d2
Xavier Maysonnave <x.maysonnave@gmail.com>
2019-10-22 12:27:34 +0530

    mimic metamask extension babelify and browserify setup

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function (api) {
+  api.cache(false)
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            browsers: [
+              'chrome >= 58',
+              'firefox >= 56.2'
+            ]
+          }
+        }
+      ]
+    ],
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-object-rest-spread'
+    ]
+  }
+}
