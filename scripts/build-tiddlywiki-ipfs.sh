651e11472eed94480a1d9dcdd7c1dcf131dfcd0e
Xavier Maysonnave <x.maysonnave@gmail.com>
2019-12-09 14:04:21 +0530

    build a dev version

--- scripts/build-tiddlywiki-ipfs.sh
+++ scripts/build-tiddlywiki-ipfs.sh
@@ -4,4 +4,10 @@ npx tiddlywiki build \
   --build \
   --verbose || exit 1
 
+node ./node_modules/tiddlywiki/tiddlywiki.js build \
+	--verbose \
+	--output wiki \
+	--rendertiddler $:/core/save/all dev.html text/plain \
+	|| exit 1
+
   exit 0
