5173c84f82691d6bc4127d095c7f5ae6d8a074ee
Xavier Maysonnave <x.maysonnave@gmail.com>
2020-03-06 14:20:47 +0530

    upgrade locator to 2.0.0

--- scripts/run-prepare-clone.sh
+++ scripts/run-prepare-clone.sh
@@ -4,7 +4,7 @@ rm -f -R ./tw5-locator > /dev/null 2>&1
 # tw5-locator
 git clone https://github.com/bimlas/tw5-locator
 cd tw5-locator
-git checkout tags/v1.5.0 > /dev/null 2>&1
+git checkout tags/v2.0.0 > /dev/null 2>&1
 cd ..
 
 exit 0
