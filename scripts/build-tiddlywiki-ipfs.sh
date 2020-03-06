39f457b95a7b381c7c45a2b3a025e6204774dd9d
Xavier Maysonnave <x.maysonnave@gmail.com>
2020-03-06 14:21:30 +0530

    provide an empty wiki

--- scripts/build-tiddlywiki-ipfs.sh
+++ scripts/build-tiddlywiki-ipfs.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+# metadata
+cp ./metadata/empty-tiddlywiki.info ./build/tiddlywiki.info > /dev/null 2>&1
+# build
+npx tiddlywiki build \
+  --output wiki \
+  --build \
+  --verbose \
+|| exit 1
+
+# bluelightav directory
+mkdir -p ./build/tiddlers > /dev/null 2>&1
+# bluelightav tiddlers
+cp -R ./tiddlers/wiki/* ./build/tiddlers > /dev/null 2>&1
+# metadata
+cp ./metadata/bluelightav-tiddlywiki.info ./build/tiddlywiki.info > /dev/null 2>&1
+# tw5-locator
+cp -R ./tw5-locator/plugins/locator ./build/plugins/locator > /dev/null 2>&1
+# build
 npx tiddlywiki build \
   --output wiki \
   --build \
--- scripts/run-prepare.sh
+++ scripts/run-prepare.sh
@@ -3,28 +3,16 @@
 rm -f -R ./build > /dev/null 2>&1
 # plugin directory
 mkdir -p ./build/plugins/ipfs/files > /dev/null 2>&1
-# wiki directory
-mkdir -p ./build/tiddlers > /dev/null 2>&1
 # ipfs scripts
 cp -R ./src/* ./build/plugins/ipfs > /dev/null 2>&1
 # plugin tiddlers
 cp -R ./tiddlers/plugin/* ./build/plugins/ipfs > /dev/null 2>&1
-# wiki tiddlers
-cp -R ./tiddlers/wiki/* ./build/tiddlers > /dev/null 2>&1
-# metadata
-cp ./metadata/tiddlywiki.info ./build > /dev/null 2>&1
 # modules
 cp ./metadata/tiddlywiki.files ./build/plugins/ipfs/files > /dev/null 2>&1
 # loglevel
 mkdir -p ./build/plugins/ipfs/files/loglevel > /dev/null 2>&1
 cp -R ./node_modules/loglevel/lib/loglevel.js ./build/plugins/ipfs/files/loglevel > /dev/null 2>&1
 cp -R ./node_modules/loglevel/LICENSE-MIT ./build/plugins/ipfs/files/loglevel > /dev/null 2>&1
-# babel-polyfill
-mkdir -p ./build/plugins/ipfs/files/babel-polyfill > /dev/null 2>&1
-cp -R ./node_modules/@babel/polyfill/dist/polyfill.min.js ./build/plugins/ipfs/files/babel-polyfill > /dev/null 2>&1
-cp -R ./node_modules/@babel/polyfill/LICENSE ./build/plugins/ipfs/files/babel-polyfill > /dev/null 2>&1
-# tw5-locator
-cp -R ./tw5-locator/plugins/locator ./build/plugins/locator > /dev/null 2>&1
 # generate build number
 ./scripts/run-build-number.sh || exit 1
 
