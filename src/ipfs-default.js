034f44e9ee22031e8e504cd3f16028b92d58c024
Xavier Maysonnave <x.maysonnave@gmail.com>
2020-03-16 10:50:37 +0530

    documentation update

--- src/ipfs-default.js
+++ src/ipfs-default.js
@@ -8,265 +8,263 @@ utils
 
 \*/
 
-(function(){
+(function() {
+  /*jslint node: true, browser: true */
+  /*global $tw: false */
+  "use strict";
 
-/*jslint node: true, browser: true */
-/*global $tw: false */
-"use strict";
+  const ipfsDefaultName = "ipfs-default";
 
-const ipfsDefaultName = "ipfs-default";
-
-/*
- * Retrieve ipfs saver export protocol with default value if applicable
- */
-exports.getIpfsExport = function() {
-  var output = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/export");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      output = text.trim();
+  /*
+   * Retrieve ipfs saver export protocol with default value if applicable
+   */
+  exports.getIpfsExport = function() {
+    var output = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/export");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        output = text.trim();
+      }
     }
-  }
-  if (output === null) {
-    output = $tw.utils.getIpfsDefaultExport();
-  }
-  return output;
-}
-
-/*
- * Default Export
- */
-exports.getIpfsDefaultExport = function() {
-  return "json";
-}
-
-/*
- * Retrieve ipfs saver protocol with default value if applicable
- */
-exports.getIpfsProtocol = function() {
-  var protocol = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/protocol");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      protocol = text.trim();
+    if (output === null) {
+      output = $tw.utils.getIpfsDefaultExport();
     }
-  }
-  if (protocol === null) {
-    protocol = $tw.utils.getIpfsDefaultProtocol();
-  }
-  return protocol;
-}
+    return output;
+  };
 
-/*
- * Default Protocol
- */
-exports.getIpfsDefaultProtocol = function() {
-  return "ipfs";
-}
+  /*
+   * Default Export
+   */
+  exports.getIpfsDefaultExport = function() {
+    return "json";
+  };
 
-/*
- * Retrieve ipfs saver api url with default value if applicable
- */
-exports.getIpfsSaverApiUrl = function() {
-  var api = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/api");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text =tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      api = text.trim();
+  /*
+   * Retrieve ipfs saver protocol with default value if applicable
+   */
+  exports.getIpfsProtocol = function() {
+    var protocol = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/protocol");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        protocol = text.trim();
+      }
     }
-  }
-  if (api !== null) {
-    tiddler = $tw.wiki.getTiddler(api);
+    if (protocol === null) {
+      protocol = $tw.utils.getIpfsDefaultProtocol();
+    }
+    return protocol;
+  };
+
+  /*
+   * Default Protocol
+   */
+  exports.getIpfsDefaultProtocol = function() {
+    return "ipfs";
+  };
+
+  /*
+   * Retrieve ipfs saver api url with default value if applicable
+   */
+  exports.getIpfsSaverApiUrl = function() {
+    var api = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/api");
     if (tiddler !== undefined && tiddler !== null) {
       const text = tiddler.getFieldString("text");
       if (text !== undefined && text !== null && text.trim() !== "") {
         api = text.trim();
       }
     }
-  }
-  if (api === null) {
-    api = $tw.utils.getIpfsDefaultApiUrl();
-  }
-  return api;
-}
-
-/*
- * Default API URL
- */
-exports.getIpfsDefaultApiUrl = function() {
-  return "https://ipfs.infura.io:5001";
-}
-
-/*
- * Retrieve ipfs saver gateway url with default value if applicable
- */
-exports.getIpfsSaverGatewayUrl = function() {
-  var gateway = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/gateway");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      gateway = text.trim();
+    if (api !== null) {
+      tiddler = $tw.wiki.getTiddler(api);
+      if (tiddler !== undefined && tiddler !== null) {
+        const text = tiddler.getFieldString("text");
+        if (text !== undefined && text !== null && text.trim() !== "") {
+          api = text.trim();
+        }
+      }
     }
-  }
-  if (gateway !== null) {
-    tiddler = $tw.wiki.getTiddler(gateway);
-    if ($tw.wiki.getTiddler(gateway) !== undefined) {
-      const text = $tw.wiki.getTiddler(gateway).getFieldString("text");
+    if (api === null) {
+      api = $tw.utils.getIpfsDefaultApiUrl();
+    }
+    return api;
+  };
+
+  /*
+   * Default API URL
+   */
+  exports.getIpfsDefaultApiUrl = function() {
+    return "https://ipfs.infura.io:5001";
+  };
+
+  /*
+   * Retrieve ipfs saver gateway url with default value if applicable
+   */
+  exports.getIpfsSaverGatewayUrl = function() {
+    var gateway = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/gateway");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
       if (text !== undefined && text !== null && text.trim() !== "") {
         gateway = text.trim();
       }
     }
-  }
-  if (gateway === null) {
-    gateway = $tw.utils.getIpfsDefaultGatewayUrl();
-  }
-  return gateway;
-}
-
-/*
- * Default Gateway URL
- */
-exports.getIpfsDefaultGatewayUrl = function() {
-  return "https://ipfs.infura.io";
-}
-
-/*
- * Retrieve ipfs saver ens domain with default value if applicable
- */
-exports.getIpfsEnsDomain = function() {
-  var ensDomain = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/ens/domain");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      ensDomain = text.trim();
+    if (gateway !== null) {
+      tiddler = $tw.wiki.getTiddler(gateway);
+      if ($tw.wiki.getTiddler(gateway) !== undefined) {
+        const text = $tw.wiki.getTiddler(gateway).getFieldString("text");
+        if (text !== undefined && text !== null && text.trim() !== "") {
+          gateway = text.trim();
+        }
+      }
     }
-  }
-  return ensDomain;
-}
-
-/*
- * Retrieve ipfs saver ipns name with default value if applicable
- */
-exports.getIpfsIpnsName = function() {
-  var ipnsName = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/ipns/name");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      ipnsName = text.trim();
+    if (gateway === null) {
+      gateway = $tw.utils.getIpfsDefaultGatewayUrl();
     }
-  }
-  return ipnsName;
-}
+    return gateway;
+  };
 
-/*
- * Retrieve ipfs saver ipns key with default value if applicable
- */
-exports.getIpfsIpnsKey = function() {
-  var ipnsKey = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/ipns/key");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      ipnsKey = text.trim();
+  /*
+   * Default Gateway URL
+   */
+  exports.getIpfsDefaultGatewayUrl = function() {
+    return "https://ipfs.infura.io";
+  };
+
+  /*
+   * Retrieve ipfs saver ens domain with default value if applicable
+   */
+  exports.getIpfsEnsDomain = function() {
+    var ensDomain = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/ens/domain");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        ensDomain = text.trim();
+      }
     }
-  }
-  return ipnsKey;
-}
+    return ensDomain;
+  };
 
-/*
- * Retrieve ipfs saver verbose with default value if applicable
- */
-exports.getIpfsVerbose = function() {
-  var verbose = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/verbose");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      verbose = text.trim();
+  /*
+   * Retrieve ipfs saver ipns name with default value if applicable
+   */
+  exports.getIpfsIpnsName = function() {
+    var ipnsName = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/ipns/name");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        ipnsName = text.trim();
+      }
     }
-  }
-  if (verbose === null) {
-    verbose = true; // default, see ipfs-saver.tid
-  } else {
-    verbose = ( verbose === "yes" );
-  }
-  return verbose;
-}
+    return ipnsName;
+  };
 
-/*
- * Retrieve ipfs saver unpin with default value if applicable
- */
-exports.getIpfsUnpin = function() {
-  var unpin = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/unpin");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      unpin = text.trim();
+  /*
+   * Retrieve ipfs saver ipns key with default value if applicable
+   */
+  exports.getIpfsIpnsKey = function() {
+    var ipnsKey = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/ipns/key");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        ipnsKey = text.trim();
+      }
     }
-  }
-  if (unpin === null) {
-    unpin = false; // default, see ipfs-saver.tid
-  } else {
-    unpin = ( unpin === "yes" );
-  }
-  return unpin;
-}
+    return ipnsKey;
+  };
 
-/*
- * Retrieve ipfs saver url policy with default value if applicable
- */
-exports.getIpfsUrlPolicy = function() {
-  var policy = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/policy");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      policy = text.trim();
+  /*
+   * Retrieve ipfs saver verbose with default value if applicable
+   */
+  exports.getIpfsVerbose = function() {
+    var verbose = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/verbose");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        verbose = text.trim();
+      }
     }
-  }
-  if (policy === null) {
-    policy = $tw.utils.getIpfsDefaultPolicy();
-  }
-  return policy;
-}
-
-/*
- * Default Policy
- */
-exports.getIpfsDefaultPolicy = function() {
-  return "origin";
-}
-
-/*
- * Retrieve ipfs saver provider with default value if applicable
- */
-exports.getIpfsProvider = function() {
-  var provider = null;
-  var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/provider");
-  if (tiddler !== undefined && tiddler !== null) {
-    const text = tiddler.getFieldString("text");
-    if (text !== undefined && text !== null && text.trim() !== "") {
-      provider = text.trim();
+    if (verbose === null) {
+      verbose = true; // default, see ipfs-saver.tid
+    } else {
+      verbose = verbose === "yes";
     }
-  }
-  if (provider === null) {
-    provider = $tw.utils.getIpfsDefaultProvider();
-  }
-  return provider;
-}
+    return verbose;
+  };
 
-/*
- * Default Provider
- */
-exports.getIpfsDefaultProvider = function() {
-  return "http";
-}
+  /*
+   * Retrieve ipfs saver unpin with default value if applicable
+   */
+  exports.getIpfsUnpin = function() {
+    var unpin = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/unpin");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        unpin = text.trim();
+      }
+    }
+    if (unpin === null) {
+      unpin = false; // default, see ipfs-saver.tid
+    } else {
+      unpin = unpin === "yes";
+    }
+    return unpin;
+  };
 
+  /*
+   * Retrieve ipfs saver url policy with default value if applicable
+   */
+  exports.getIpfsUrlPolicy = function() {
+    var policy = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/policy");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        policy = text.trim();
+      }
+    }
+    if (policy === null) {
+      policy = $tw.utils.getIpfsDefaultPolicy();
+    }
+    return policy;
+  };
+
+  /*
+   * Default Policy
+   */
+  exports.getIpfsDefaultPolicy = function() {
+    return "gateway";
+  };
+
+  /*
+   * Retrieve ipfs saver provider with default value if applicable
+   */
+  exports.getIpfsProvider = function() {
+    var provider = null;
+    var tiddler = $tw.wiki.getTiddler("$:/ipfs/saver/provider");
+    if (tiddler !== undefined && tiddler !== null) {
+      const text = tiddler.getFieldString("text");
+      if (text !== undefined && text !== null && text.trim() !== "") {
+        provider = text.trim();
+      }
+    }
+    if (provider === null) {
+      provider = $tw.utils.getIpfsDefaultProvider();
+    }
+    return provider;
+  };
+
+  /*
+   * Default Provider
+   */
+  exports.getIpfsDefaultProvider = function() {
+    return "http";
+  };
 })();
