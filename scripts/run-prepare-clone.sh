306334bd4765e7b212761b2f7391e758c3b569aa
Xavier Maysonnave <x.maysonnave@gmail.com>
2020-06-24 09:26:27 +0530

    output command rather than silent

--- scripts/run-prepare-clone.sh
+++ scripts/run-prepare-clone.sh
@@ -6,13 +6,13 @@ rm -f -R ./tw5-relink > /dev/null 2>&1
 # tw5-locator
 git clone https://github.com/bimlas/tw5-locator
 cd tw5-locator
-git checkout tags/v2.0.0 > /dev/null 2>&1
+git checkout tags/v2.0.0
 cd ..
 
 # tw5-relink
 git clone https://github.com/flibbles/tw5-relink
 cd tw5-relink
-git checkout tags/v1.10.0 > /dev/null 2>&1
+git checkout tags/v1.10.0
 cd ..
 
 exit 0
--- scripts/run-prepare.sh
+++ scripts/run-prepare.sh
@@ -6,9 +6,6 @@ rm -f -R ./output > /dev/null 2>&1
 # build directory
 mkdir -p ./build/plugins/ipfs/core > /dev/null 2>&1
 
-# temp output directory
-mkdir -p ./build/output > /dev/null 2>&1
-
 # core scripts
 cp -R ./src/core/* ./build/plugins/ipfs/core > /dev/null 2>&1
 
@@ -23,8 +20,8 @@ cp ./metadata/tiddlywiki.files ./build/plugins/ipfs/files > /dev/null 2>&1
 
 # loglevel
 cp -R ./loglevel ./build/plugins > /dev/null 2>&1
-wget https://cdn.jsdelivr.net/npm/loglevel@1.6.8/lib/loglevel.min.js -P ./build/plugins/loglevel > /dev/null 2>&1
-wget https://raw.githubusercontent.com/pimterry/loglevel/master/LICENSE-MIT -P ./build/plugins/loglevel > /dev/null 2>&1
+wget https://cdn.jsdelivr.net/npm/loglevel@1.6.8/lib/loglevel.min.js -P ./build/plugins/loglevel
+wget https://raw.githubusercontent.com/pimterry/loglevel/master/LICENSE-MIT -P ./build/plugins/loglevel
 
 # generate build number
 ./scripts/run-build-number.sh \
