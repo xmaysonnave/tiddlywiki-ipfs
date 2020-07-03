934b8469784f3866da7d9a2f898c4501cc106274
Xavier Maysonnave <x.maysonnave@gmail.com>
2020-07-03 13:42:25 +0530

    ethereum encryption support

---
+++ src/ipfs-encryption-tooltip.js
@@ -0,0 +1,39 @@
+/*\
+title: $:/plugins/ipfs/macro/ipfs-console-tooltip.js
+type: application/javascript
+tags: $:/ipfs/core
+module-type: macro
+
+IPFS plugin version
+
+\*/
+
+;(function () {
+  /*jslint node: true, browser: true */
+  /*global $tw: false */
+  'use strict'
+
+  /*
+   * Information about this macro
+   */
+  exports.name = 'ipfs-encryption-tooltip'
+
+  exports.params = []
+
+  /*
+   * Run the macro
+   */
+  exports.run = function () {
+    var tiddler = $tw.wiki.getTiddler('$:/language/Buttons/Console/Mobile/Hint')
+    if (typeof window.eruda === 'undefined') {
+      tiddler = $tw.wiki.getTiddler('$:/language/Buttons/Console/Mobile/Load')
+    } else if ($tw.ipfs.ipfsAction !== undefined) {
+      if ($tw.ipfs.ipfsAction.console) {
+        tiddler = $tw.wiki.getTiddler('$:/language/Buttons/Console/Mobile/Hide')
+      } else {
+        tiddler = $tw.wiki.getTiddler('$:/language/Buttons/Console/Mobile/Show')
+      }
+    }
+    return tiddler.fields.text
+  }
+})()
