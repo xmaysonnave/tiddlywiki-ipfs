c0150d2552415996616d04c708f93000f580b720
Xavier Maysonnave <x.maysonnave@gmail.com>
2020-07-11 13:10:38 +0530

    improve inner exception handling fix button widget tooltip

---
+++ TiddlyWiki5/core/modules/startup/password.js
@@ -0,0 +1,101 @@
+/*\
+title: $:/core/modules/startup/password.js
+type: application/javascript
+module-type: startup
+
+Compression handling
+
+\*/
+;(function () {
+  /*jslint node:true,browser:true*/
+  /*global $tw:false*/
+  'use strict'
+
+  // Export name and synchronous status
+  exports.name = 'password'
+  exports.platforms = ['browser']
+  exports.after = ['startup']
+  exports.synchronous = true
+
+  exports.startup = function () {
+    var setPassword = function () {
+      $tw.passwordPrompt.createPrompt({
+        serviceName: $tw.language.getString('Encryption/PromptSetPassword'),
+        noUserName: true,
+        submitText: $tw.language.getString('Encryption/SetPassword'),
+        canCancel: true,
+        repeatPassword: true,
+        callback: function (data) {
+          if (data) {
+            $tw.crypto.setPassword(data.password)
+          } else {
+            $tw.crypto.setPassword(null)
+          }
+          return true // Get rid of the password prompt
+        }
+      })
+    }
+    // Ensure that $:/isEncrypted is maintained properly
+    $tw.wiki.addEventListener('change', function (changes) {
+      if ($tw.utils.hop(changes, '$:/config/Standford')) {
+        const encrypted = $tw.wiki.getTiddler('$:/isEncrypted')
+        if (encrypted.fields.text === 'yes') {
+          const hasPassword = $tw.crypto.hasPassword()
+          const standford = $tw.wiki.getTiddler('$:/config/Standford')
+          if (!hasPassword && standford.fields.text === 'yes') {
+            setPassword()
+          } else if (hasPassword) {
+            $tw.rootWidget.dispatchEvent({ type: 'tm-clear-password' })
+          }
+        }
+      }
+    })
+    $tw.rootWidget.addEventListener('tm-set-password', async function (event) {
+      const standford = $tw.wiki.getTiddler('$:/config/Standford')
+      if (standford.fields.text === 'yes') {
+        if ($tw.crypto.hasPassword() === false) {
+          setPassword()
+        }
+      } else {
+        try {
+          const encryptionKey = await $tw.ipfs.getPublicEncryptionKey()
+          $tw.crypto.setEncryptionKey(encryptionKey)
+        } catch (error) {
+          if (error.name !== 'RejectedUserRequest') {
+            this.getLogger().error(error)
+          }
+          $tw.utils.alert(name, error.message)
+          $tw.crypto.setEncryptionKey(null)
+        }
+      }
+    })
+    $tw.rootWidget.addEventListener('tm-clear-password', async function (
+      event
+    ) {
+      if ($tw.browser) {
+        const hadPassword = $tw.crypto.hasPassword()
+        const standford = $tw.wiki.getTiddler('$:/config/Standford')
+        if (
+          hadPassword &&
+          standford.fields.text === 'yes' &&
+          !confirm($tw.language.getString('Encryption/ConfirmClearPassword'))
+        ) {
+          return
+        }
+        $tw.crypto.setPassword(null)
+        if (standford.fields.text === 'no' && hadPassword) {
+          try {
+            const encryptionKey = await $tw.ipfs.getPublicEncryptionKey()
+            $tw.crypto.setEncryptionKey(encryptionKey)
+          } catch (error) {
+            if (error.name !== 'RejectedUserRequest') {
+              this.getLogger().error(error)
+            }
+            $tw.utils.alert(name, error.message)
+            $tw.crypto.setEncryptionKey(null)
+          }
+        }
+      }
+    })
+  }
+})()
