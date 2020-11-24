5d0d407fa60ddaebab05beed7f41aa2e35fb8681
Xavier Maysonnave <x.maysonnave@gmail.com>
2020-11-24 14:48:45 +0530

    test build

--- scripts/editions/build.sh
+++ scripts/editions/build.sh
@@ -8,12 +8,12 @@ echo 'nvm:' $(nvm -v)
 nvm use
 
 # build empty
-./scripts/editions/empty/build.sh "$@" || exit 1
-node ./scripts/editions/empty/upload.js "$@" || exit 1
+# ./scripts/editions/empty/build.sh "$@" || exit 1
+# node ./scripts/editions/empty/upload.js "$@" || exit 1
 
 # build bluelightav.eth
-./scripts/editions/bluelightav/build.sh "$@" || exit 1
-node ./scripts/editions/bluelightav/upload.js "$@" || exit 1
+# ./scripts/editions/bluelightav/build.sh "$@" || exit 1
+# node ./scripts/editions/bluelightav/upload.js "$@" || exit 1
 
 # build dev
 ./scripts/editions/dev/build.sh "$@" || exit 1
