a2e4ca890489ef95f823f6fab05365b3eec1e14c
Xavier Maysonnave <x.maysonnave@gmail.com>
2021-04-02 16:40:32 +0530

    fix tests

--- tests/ipns.test.js
+++ tests/ipns.test.js
@@ -84,7 +84,7 @@ describe('IPNS key and IPNS name', () => {
     ipfsBundle.init()
     const ipfsWrapper = new IpfsWrapper(ipfsBundle)
     ipfsWrapper.ipfsLibrary.keyList = sinon.fake.returns(keys)
-    const { ipnsKey, ipnsName } = await ipfsWrapper.getIpnsIdentifier(null, 'k51qzi5uqu5dmdbdlz9ccv1ze114psij95j5kzqszhy952g6qllvm3x52oava0', null, null, 'tiddly', true)
+    const { ipnsKey, ipnsName } = await ipfsWrapper.getIpnsIdentifier(null, 'k51qzi5uqu5dmdbdlz9ccv1ze114psij95j5kzqszhy952g6qllvm3x52oava0', true, null, null, 'tiddly')
     expect(ipnsName === 'tiddly' && ipnsKey === 'k51qzi5uqu5dmdbdlz9ccv1ze114psij95j5kzqszhy952g6qllvm3x52oava0').to.be.true
   })
 })
@@ -95,7 +95,7 @@ describe('IPNS name', () => {
     const ipfsWrapper = new IpfsWrapper(ipfsBundle)
     ipfsWrapper.ipfsLibrary.keyList = sinon.fake.returns(keys)
     try {
-      await ipfsWrapper.getIpnsIdentifier(null, null, null, null, 'dummy', true)
+      await ipfsWrapper.getIpnsIdentifier(null, null, true, null, null, 'dummy')
     } catch (error) {
       expect(error.message).to.equal('Unknown IPNS identifier...')
     }
@@ -105,7 +105,7 @@ describe('IPNS name', () => {
     ipfsBundle.init()
     const ipfsWrapper = new IpfsWrapper(ipfsBundle)
     ipfsWrapper.ipfsLibrary.keyList = sinon.fake.returns(keys)
-    const { ipnsKey, ipnsName } = await ipfsWrapper.getIpnsIdentifier(null, null, null, null, 'tiddly', true)
+    const { ipnsKey, ipnsName } = await ipfsWrapper.getIpnsIdentifier(null, null, true, null, null, 'tiddly')
     expect(ipnsName === 'tiddly' && ipnsKey === 'k51qzi5uqu5dmdbdlz9ccv1ze114psij95j5kzqszhy952g6qllvm3x52oava0').to.be.true
   })
 })
